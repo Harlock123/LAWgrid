fb247dfc7c2596e0fa14326b8e77f2dfadc93105
lwatson <harlock123@gmail.com>
2025-10-27 05:18:05 -0400

    Add button click handlers and update grid population logic in `GridTestBed`
    - Linked `cmdTest1`, `cmdTest2`, and `cmdTest3` to respective click event handlers.
    - Implemented SQL grid population logic in `cmdTest1` click handler using a test query.
    - Updated button definitions in `MainWindow.axaml` with descriptive names.

--- GridTestBed/MainWindow.axaml.cs
+++ GridTestBed/MainWindow.axaml.cs
@@ -1,4 +1,5 @@
 using Avalonia.Controls;
+using Avalonia.Interactivity;
 
 namespace GridTestBed;
 
@@ -8,6 +9,31 @@ public partial class MainWindow : Window
     {
         InitializeComponent();
         
+        cmdTest1.Click += CmdTest1OnClick;
+        cmdTest2.Click += CmdTest2OnClick;
+        cmdTest3.Click += CmdTest3OnClick;
+        
         TheGridInTest.TestPopulate();
+    }
+
+    private void CmdTest3OnClick(object? sender, RoutedEventArgs e)
+    {
+        
+    }
+
+    private void CmdTest2OnClick(object? sender, RoutedEventArgs e)
+    {
+        
+    }
+
+    private void CmdTest1OnClick(object? sender, RoutedEventArgs e)
+    {
+        string ConnectionString = "Server=luisbhds.database.windows.net;Database=LUISBHDS_TEST;User Id={MyUserName};Password={MyPasswordHere}";
+        
+        string Query = "SELECT top 100 * FROM dbo.MEMBERMAIN";
+
+        TheGridInTest.PopulateFromSqlQuerySync(ConnectionString, Query);
+        
+
     }
 }
